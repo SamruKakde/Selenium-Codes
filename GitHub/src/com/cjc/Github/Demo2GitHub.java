c89849b0a5918d2abc5c82f5ef9b20fa929ff450
Samruddhi <Samruddhi@DESKTOP-O5VFRGR>
2021-07-30 15:49:20 +0530

    2nd class commit

---
+++ GitHub/src/com/cjc/Github/Demo2GitHub.java
@@ -0,0 +1,11 @@
+package com.cjc.Github;
+
+public class Demo2GitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Class 2 in GitHub..");
+	}
+
+}
