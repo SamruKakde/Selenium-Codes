2c117eeb70c59355ede633e385351bf6b6291fac
Samruddhi <Samruddhi@DESKTOP-O5VFRGR>
2021-07-30 15:55:17 +0530

    New Package commit by Samruddhi

---
+++ GitHub/src/com/cjc/Git2/DemoGit1.java
@@ -0,0 +1,11 @@
+package com.cjc.Git2;
+
+public class DemoGit1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("New Package and class commit in GitHub..");
+
+	}
+
+}
