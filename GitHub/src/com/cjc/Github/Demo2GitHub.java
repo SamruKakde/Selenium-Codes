1ba444728071b3fa02edb0a1dfb41bc1488c6594
Samruddhi <Samruddhi@DESKTOP-O5VFRGR>
2021-07-31 15:28:21 +0530

    Error correction commit

--- GitHub/src/com/cjc/Github/Demo2GitHub.java
+++ GitHub/src/com/cjc/Github/Demo2GitHub.java
@@ -6,11 +6,12 @@ public class Demo2GitHub {
 		// TODO Auto-generated method stub
 
 		System.out.println("Class 2 in GitHub..");
+	}
 		
 		public void m1()
 		{
 		  System.out.println("Github patch1");
 		}
-	}
+	
 
 }
